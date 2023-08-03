56fb652c867c22e76f2c0813ed9544d86cd1d2dd
TheYeti2121 <103778301+TheYeti2121@users.noreply.github.com>
2023-08-03 15:57:46 +0100

    Add files via upload

---
+++ BulkFaceUpload.py
@@ -0,0 +1,16 @@
+import boto3
+
+s3 = boto3.resource('s3')
+
+images =  [('1.JPEG', 'Lionel Messi'),
+           ('2.PNG', 'Cristiano Ronaldo'),
+           ('3.JPEG', 'Elon Musk'),
+           ('4.JPEG', ' Donald Trump'),
+           ('5.JPEG', 'Boris Johnson')]
+
+for image in images:
+    file = open(image[0],'rb')
+    object = s3.Object('rekognitionfacecollection','index/'+ image[0])
+    ret = object.put(Body=file,
+                     Metadata={'FullName':image[1]}
+                     )
