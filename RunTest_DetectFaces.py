808fb7313f4dda4f6d2f772c99c0625f6054ff75
TheYeti2121 <103778301+TheYeti2121@users.noreply.github.com>
2023-08-03 17:39:29 +0100

    Update RunTest_DetectFaces.py

--- RunTest_DetectFaces.py
+++ RunTest_DetectFaces.py
@@ -1,33 +1,30 @@
 import boto3
+import io
+from PIL import Image
 
-def search_faces_by_image(bucket, key, collection_id, threshold=80, region="us-west-2"):
-    rekognition = boto3.client("rekognition", region)
+rekognition = boto3.client('rekognition', region_name='eu-west-2')
+dynamodb = boto3.client('dynamodb', region_name='eu-west-2')
     
-    response = rekognition.search_faces_by_image(
-        CollectionId=collection_id,
-        Image={
-            "S3Object": {
-                "Bucket": bucket,
-                "Name": key,
-            }
-        },
-        FaceMatchThreshold=threshold,
-    )
+image = Image.open("Test2.jpg")
+stream = io.BytesIO()
+image.save(stream,format="JPEG")
+image_binary = stream.getvalue()
+
+
+response = rekognition.search_faces_by_image(
+        CollectionId='family_collection',
+        Image={'Bytes':image_binary}                                       
+        )
     
-    face_matches = response['FaceMatches']
+for match in response['FaceMatches']:
+    print (match['Face']['FaceId'],match['Face']['Confidence'])
+        
+    face = dynamodb.get_item(
+        TableName='family_collection',  
+        Key={'RekognitionId': {'S': match['Face']['FaceId']}}
+        )
     
-    print(f"Matching faces")
-    for match in face_matches:
-        print(f"FaceId: {match['Face']['FaceId']}")
-        print(f"Similarity: {match['Similarity']}")
-
-    if not face_matches:
-        print("No matches found")
-
-# Assuming you already have a bucket and an image key (file path)
-bucket = "your_bucket_name"
-key = "your_image_file"
-collection_id = "your_collection_id"
-
-# Call the function
-search_faces_by_image(bucket, key, collection_id)
+    if 'Item' in face:
+        print ("The face identified is:", face['Item']['FullName']['S'])
+    else:
+        print ('no match found in person lookup')
